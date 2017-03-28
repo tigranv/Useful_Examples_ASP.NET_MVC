0dd800fba66dfad3aa2fb456e8d9c658a9ef3192
Tigran Vardanyan <vardanyant.89@gmail.com>
2017-03-28 13:08:25 +0400

    core page

---
+++ ASP.NET Page Class/PageEvenst/AllPageEvents.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="AllPageEvents.aspx.cs" Inherits="PageEvenst.AllPageEvents" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Label ID="Label1" runat="server" EnableViewState="false" />
+        <asp:Button ID="Button1" Text="Button" runat="server" OnClick="Button1_Click" />    </div>
+    </form>
+</body>
+</html>
---
+++ ASP.NET Page Class/PageEvenst/AllPageEvents.aspx.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PageEvenst
+{
+    public partial class AllPageEvents : System.Web.UI.Page
+    {
+        protected void Page_PreInit(object sender, EventArgs e)
+        {
+            Label1.Text += "PreInit <b>IsPostBack " + IsPostBack + "</b><br />";
+        }
+
+        protected void Page_Init(object sender, EventArgs e)
+        {
+            Label1.Text += "Init <br />";
+        }
+
+        protected void Page_InitComplete(object sender, EventArgs e)
+        {
+            Label1.Text += "InitComplete <br />";
+        }
+
+        protected void Page_PreLoad(object sender, EventArgs e)
+        {
+            Label1.Text += "PreLoad <br />";
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Label1.Text += "Load <br />";
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Label1.Text += "<b>The handler of the button clicked</b><br />";
+        }
+
+        protected void Page_LoadComplete(object sender, EventArgs e)
+        {
+            Label1.Text += "LoadComplete <br />";
+        }
+
+        protected void Page_PreRender(object sender, EventArgs e)
+        {
+            Label1.Text += "PreRender <br />";
+        }
+
+        protected void Page_PreRenderComplete(object sender, EventArgs e)
+        {
+            Label1.Text += "PreRenderComplete <br />";
+        }
+
+        protected void Page_SaveStateComplete(object sender, EventArgs e)
+        {
+            Label1.Text += "SaveStateComplete <br />";
+        }
+
+        // Rendering the page. All controls are turned into HTML, CSS and JavaScript, which will be sent to the client.
+
+        protected void Page_Unload(object sender, EventArgs e)
+        {
+            // Release the resources that used the page.        }
+        }
+
+    }
+}
