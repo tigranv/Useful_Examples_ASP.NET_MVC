e168659cda7b6a22840c3ee30c73a211cf397ee7
Tigran Vardanyan <vardanyant.89@gmail.com>
2017-03-31 19:37:56 +0400

    area added admin

---
+++ MyFirstMVC/Areas/Areas/Admin/AdminAreaRegistration.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Areas.Areas.Admin
+{
+    public class AdminAreaRegistration
+    {
+    }
+}
---
+++ MyFirstMVC/Areas/Areas/Admin/Controllers/ProductsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Areas.Areas.Admin.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Admin/Products
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        public ActionResult Delete()
+        {
+            return View();
+        }
+    }
+}
