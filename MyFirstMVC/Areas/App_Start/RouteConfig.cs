0910a1800eaa05264bf8e698182f589c41925ca4
Tigran Vardanyan <vardanyant.89@gmail.com>
2017-03-31 19:49:42 +0400

    b are added to area admin

--- MyFirstMVC/Areas/App_Start/RouteConfig.cs
+++ MyFirstMVC/Areas/App_Start/RouteConfig.cs
@@ -16,7 +16,8 @@ namespace Areas
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new[] { "Areas.Controllers" }
             );
         }
     }
