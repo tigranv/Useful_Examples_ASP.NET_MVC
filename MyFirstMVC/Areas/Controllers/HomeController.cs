09a2d5fd0ec632349afe84f02911ca4fc23d775e
Tigran Vardanyan <vardanyant.89@gmail.com>
2017-03-31 19:29:41 +0400

    Areas core

---
+++ MyFirstMVC/Areas/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Areas.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Contacts()
+        {
+            return View();
+        }
+      
+    }
+}
---
+++ MyFirstMVC/Areas/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Areas.MvcApplication" Language="C#" %>
