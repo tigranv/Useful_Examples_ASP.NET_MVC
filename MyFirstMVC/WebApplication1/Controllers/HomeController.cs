4674658917fefe15701027082efeae86281e00f8
Tigran Vardanyan <vardanyant.89@gmail.com>
2017-03-31 14:59:18 +0400

    Controller added

---
+++ MyFirstMVC/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            List<Product> products = new List<Product>();
+
+            products.Add(new Product()
+            {
+                ID = 1,
+                Name = "Computer",
+                Price = 1500,
+                Description = "Laptop"
+
+            });
+
+            products.Add(new Product()
+            {
+                ID = 2,
+                Name = "Mous",
+                Price = 150,
+                Description = "bluetooth"
+
+            });
+
+
+            return View(products);
+        }
+
+    }
+}
