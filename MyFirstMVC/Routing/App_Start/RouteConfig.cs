05f9cf4e25ba043c0d62f58e8c83afad05f4a24e
Tigran Vardanyan <vardanyant.89@gmail.com>
2017-03-31 16:44:43 +0400

    Routing

---
+++ MyFirstMVC/Routing/App_Start/RouteConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Routing
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Calc",
+                url: "{controller}/{action}/{x}/{y}",
+                defaults: new { controller = "Calc", action = "Add", x = 0, y = 0}
+            );
+        }
+    }
+}
---
+++ MyFirstMVC/Routing/Controllers/CalcController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Routing.Controllers
+{
+    public class CalcController : Controller
+    {
+        // GET: Calc
+        public ActionResult Add(int? x, int? y)
+        {
+            if (x != null && y != null)
+            {
+                ViewBag.Result = x + y;
+
+            }
+            return View();
+        }
+
+        public ActionResult Mul(int? x, int? y)
+        {
+            if (x != null && y != null)
+            {
+                ViewBag.Result = x*y;
+
+            }
+            return View();
+        }
+
+        public ActionResult Sub(int? x, int? y)
+        {
+            if (x != null && y != null)
+            {
+                ViewBag.Result = x - y;
+
+            }
+            return View();
+        }
+
+        public ActionResult Div(int? x, int? y)
+        {
+            if (x != null && y != null && y != 0)
+            {
+                ViewBag.Result = x/y;
+
+            }
+            return View();
+        }
+    }
+}
