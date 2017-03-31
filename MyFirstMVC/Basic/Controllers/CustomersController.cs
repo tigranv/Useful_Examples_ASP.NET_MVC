3e7b90088f608ba6ad93e05e6331c97f43fa722d
Tigran Vardanyan <vardanyant.89@gmail.com>
2017-03-31 15:33:16 +0400

    new project, diff directories

---
+++ MyFirstMVC/Basic/Controllers/CustomersController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Basic.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Add()
+        {
+            return View();
+        }
+    }
+}
---
+++ MyFirstMVC/Basic/Controllers/ProductsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Basic.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult List()
+        {
+            return View();
+        }
+
+        public ActionResult Details()
+        {
+            return View();
+        }
+    }
+}
