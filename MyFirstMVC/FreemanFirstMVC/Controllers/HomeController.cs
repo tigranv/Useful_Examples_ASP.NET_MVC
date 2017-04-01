f0c33c474ca0fa6b4765d618cf8679c75c2ce971
Tigran Vardanyan <vardanyant.89@gmail.com>
2017-04-02 01:43:21 +0400

    core

---
+++ MyFirstMVC/FreemanFirstMVC/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FreemanFirstMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ViewResult Index()
+        {
+            return View();
+        }
+    }
+}
