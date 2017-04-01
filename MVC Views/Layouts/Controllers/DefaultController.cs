b56d27c1c44ff5aa1bb812b9515836ae89cf66a7
Tigran Vardanyan <vardanyant.89@gmail.com>
2017-04-01 16:20:20 +0400

    core

---
+++ MVC Views/Layouts/Controllers/DefaultController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Layouts.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Home()
+        {
+            return View();
+        }
+
+        public ActionResult Products()
+        {
+            return View();
+        }
+
+        public ActionResult Services()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
