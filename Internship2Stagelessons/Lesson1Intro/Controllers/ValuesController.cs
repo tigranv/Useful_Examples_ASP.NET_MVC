d4314391c2c9a77135d4c704cf12cbd8ec7429cb
Tigran Vardanyan <vardanyant.89@gmail.com>
2017-04-14 14:01:43 +0400

    core api

---
+++ Internship2Stagelessons/Lesson1Intro/Controllers/ValuesController.cs
@@ -0,0 +1,43 @@
+﻿using Lesson1Intro.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Lesson1Intro.Controllers
+{
+    [RoutePrefix("api")]
+    public class ValuesController : ApiController
+    {
+        [Route("values")]
+        [HttpGet]
+        public IHttpActionResult GetValues()
+        {
+            var result = new string[]
+            {
+                "hello world",
+                "Բարև աշխարհ"
+            };
+            return Ok(result);
+        }
+
+        [Route("values")]
+        [HttpPost]
+        public IHttpActionResult SendValues(Person model)
+        {
+            if (model.Age < 18)
+            {
+                return BadRequest();
+            }
+
+            model.Age++;
+            model.Name += "!";
+
+            
+
+            return Ok(model);
+        }
+    }
+}
---
+++ Internship2Stagelessons/Lesson1Intro/Models/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lesson1Intro.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
