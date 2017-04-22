7a9145a7e505995d03733f6401ceb9011522cdb9
narekye <yegoryan.narek@gmail.com>
2017-04-21 21:56:43 -0600

    upcoming commit

--- Internship2Stagelessons/Picturesfromurl/Controllers/PicturesExtracterController.cs
+++ Internship2Stagelessons/Picturesfromurl/Controllers/PicturesExtracterController.cs
@@ -1,16 +1,11 @@
 ﻿using Picturesfromurl.Models;
 using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Net.Http.Formatting;
-using System.Net.Http.Headers;
 using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 using System.Web.Http;
-using Newtonsoft.Json;
-using Picturesfromurl.Formatter;
 
 namespace Picturesfromurl.Controllers
 {
@@ -23,13 +18,10 @@ namespace Picturesfromurl.Controllers
         // GET: api/PicturesExtracter/url
         public IHttpActionResult Get(string url)
         {
-            //if (!Uri.IsWellFormedUriString(url, UriKind.RelativeOrAbsolute))
-            //    return BadRequest("Please enter valid URL!!!");
-            //else if (!url.Contains("http"))
-            //    return BadRequest("Please include \"Http\" URL!!!");
-            //else
-            //{
-            string path = url;
+            if (!Uri.IsWellFormedUriString(url, UriKind.RelativeOrAbsolute))
+                return BadRequest("Please enter valid URL!!!");
+            if (!url.Contains("http"))
+                return BadRequest("Please include \"Http\" URL!!!");
             using (client = new WebClient())
             {
                 HtmlContent = client.DownloadString(url);
@@ -37,52 +29,31 @@ namespace Picturesfromurl.Controllers
 
             AllUrls = new List<string>();
 
-            var imageURLs = showMatch(HtmlContent, @"<(img)\b[^>]*>");
-            string[] split = imageURLs.Split(new Char[] { '"', '?' });
+            var imageUrLs = showMatch(HtmlContent, @"<(img)\b[^>]*>");
+            string[] split = imageUrLs.Split(new Char[] { '"', '?' });
 
             foreach (var item in split)
             {
-
                 if (item.Contains(".jpg") || item.Contains(".png") || item.Contains(".svg"))
                 {
                     AllUrls.Add(item);
                 }
             }
-            //}
-            ImageFormatter d = new ImageFormatter();
-            
-            return MM(AllUrls);
+            return Excellent_Method(AllUrls).Result;
         }
 
-        public IHttpActionResult MM(List<string> coll)
+        public async Task<IHttpActionResult> Excellent_Method(List<string> coll)
         {
             HttpClient client = new HttpClient();
             HttpResponseMessage mes = new HttpResponseMessage();
-            List<byte[]> list = new List<byte[]>();
+            List<Node> list = new List<Node>();
             foreach (string s in coll)
             {
-                var data = client.GetByteArrayAsync(s).Result;
-                list.Add(data);
+                Node t = new Node { Bytes = await client.GetByteArrayAsync(s) };
+                list.Add(t);
             }
-            //mes.Content = new ObjectContent<List<byte[]>>(list, new JsonMediaTypeFormatter());
             return Ok(list);
         }
-        // image
-        //public HttpResponseMessage Get()
-        //{
-        //    HttpResponseMessage result = new HttpResponseMessage(HttpStatusCode.OK);
-        //    string path = HostingEnvironment.MapPath("~/Images/pic.jpg");
-
-        //    using (FileStream fs = new FileStream(path, FileMode.Open))
-        //    using (Image image = Image.FromStream(fs))
-        //    using (MemoryStream ms = new MemoryStream())
-        //    {
-        //        image.Save(ms, ImageFormat.Jpeg);
-        //        result.Content = new ByteArrayContent(ms.ToArray()); // binary array
-        //        result.Content.Headers.ContentType = new MediaTypeHeaderValue("image/jpeg");
-        //    }
-        //    return result;
-        //}
 
         private string showMatch(string text, string expr)
         {
@@ -90,9 +61,8 @@ namespace Picturesfromurl.Controllers
             string result = "";
             foreach (Match m in mc)
             {
-                result += m.ToString() + "\n";
+                result += m + "\n";
             }
-
             return result;
         }
     }
